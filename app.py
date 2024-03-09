04ca4b7e0fc568710dd6a902f332a3a9830ae195
lostiedekerhor <meriadecdekerhor@gmail.com>
2024-03-09 23:43:28 +0100

    - ajout de la recommandation par tag - ajout d'une mesure de surveillance de l'efficacité du model de recommandation

--- app.py
+++ app.py
@@ -6,35 +6,33 @@ import os
 from imdb.imdb import get_movie_poster
 import pandas as pd
 
-from recomendation_engine.recomandation_engine import random_engin
+from recomendation_engine.recomandation_engine import random_engin, tag_engine
+
+import json
+import random
 
 
-def load_and_resize_image(path, width, height):
-    original_image = Image.open(path)
-    resized_image = original_image.resize((width, height))
-    tk_image = ImageTk.PhotoImage(resized_image)
-    tk_image.image = tk_image
-    return tk_image
+def json_int_keys_decoder(dictionary):
+    return {int(key): value for key, value in dictionary.items()}
 
 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
+        self.__init_interface__()
 
-        # prediction
-        self.df_movies = pd.read_csv("data/clean_datas/movies_app.csv")
+        # Recommandations
+        self.recommandations = []
+        self.nbr_recommandations = 10
         self.recommandation_results = []
         self.known_ids = []
 
-        # Engines
-        self.random_engine = random_engin(self.df_movies)
-
         # datas
-        self.current_movie_name = None
-        self.current_movie_id = None
-        self.current_movie_img_url = None
-        self.current_reco_engine = None
+        self.current_recommandation = None
 
+        self.get_new_movie()
+
+    def __init_interface__(self):
         self.geometry("450x625")
         self.resizable(False, False)
         self.title("Technical Project - Movie Tinder")
@@ -65,28 +63,104 @@ class App(ctk.CTk):
                                          fg_color="gray", hover_color="gray39")
         self.unknown_btn.grid(row=2, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
 
-        # end of setup
-        self.get_new_movie()
+    def __init_data__(self):
+        self.df_movies = pd.read_csv("data/clean_datas/movies_app.csv")
 
-    def get_new_movie(self):
-        while True:
-            prediction = self.random_engine.predict()
-            current_movie_id = prediction["id"].iloc[0]
+        with open('data/clean_datas/movies_tags.json', 'r') as f:
+            self.movies_tags = json.load(f, object_hook=json_int_keys_decoder)
 
-            if current_movie_id not in self.known_ids:
+        if os.path.isfile("data/kpi_datas/recommandation_results.csv"):
+            self.user_pref = pd.read_csv("data/kpi_datas/recommandation_results.csv")
+            for id_ in self.user_pref["0"].tolist():
+                if id not in self.known_ids:
+                    self.known_ids.append(id_)
+        else:
+            self.user_pref = None
+
+    def __init_engines__(self):
+        self.random_engine = random_engin(self.df_movies)
+        self.tag_engine = tag_engine(self.df_movies, self.movies_tags, self.user_pref, self.known_ids)
+
+    # ===================================== #
+    def get_random_recommandation(self):
+        res = []
+        known_ids = []
+
+        for _ in range(self.nbr_recommandations):
+            while True:
+                recommandation = self.random_engine.predict()
+                current_movie_id = recommandation["id"].iloc[0]
+
+                if current_movie_id not in self.known_ids and current_movie_id not in known_ids:
+                    break
+
+            res.append(
+                [
+                    recommandation["id"].iloc[0],
+                    recommandation["title"].iloc[0],
+                    recommandation["poster_path"].iloc[0],
+                    "random"
+                ]
+            )
+            known_ids.append(current_movie_id)
+
+        return res
+
+    def get_tags_recommandation(self):
+        res = []
+        for id_ in self.tag_engine.predict_selection(self.nbr_recommandations):
+            recommandation = self.df_movies.loc[self.df_movies['id'] == id_]
+            res.append(
+                [
+                    recommandation["id"].iloc[0],
+                    recommandation["title"].iloc[0],
+                    recommandation["poster_path"].iloc[0],
+                    "tag"
+                ]
+            )
+
+        return res
+
+    def get_new_recommandations(self):
+        if not isinstance(self.user_pref, pd.DataFrame):
+            self.recommandations = self.get_random_recommandation()
+            return
+
+        res = []
+
+        random_reco = self.get_random_recommandation()
+        tag_reco = self.get_tags_recommandation()
+        for random_rec, tag_rec in zip(random_reco, tag_reco):
+            res.append(random_rec)
+            res.append(tag_rec)
+
+            if len(res) >= self.nbr_recommandations:
                 break
 
-        self.current_movie_id = current_movie_id
-        self.current_movie_name = prediction["title"].iloc[0]
-        self.current_movie_img_url = prediction["poster_path"].iloc[0]
+        random.shuffle(res)
+
+        self.recommandations = res
+    # ===================================== #
+
+    def get_new_movie(self):
+        if len(self.recommandations) <= 0:
+            if len(self.recommandation_results) > 0:
+                self.write_to_csv()
+
+            self.__init_data__()
+            self.__init_engines__()
+            self.get_new_recommandations()
+
+        self.current_recommandation = self.recommandations.pop(0)
 
         try:
-            img = Image.open(BytesIO(get_movie_poster(self.current_movie_img_url)))
+            img = Image.open(BytesIO(get_movie_poster(self.current_recommandation[2])))
             self.your_image = ctk.CTkImage(light_image=img, size=(333, 500))
             self.image_label.configure(image=self.your_image)
         except Exception as e:
             print(f"Error changing image: {e}")
 
+    # ===================================== #
     def write_to_csv(self):
         file_path = "data/kpi_datas/recommandation_results.csv"
 
@@ -99,21 +173,33 @@ class App(ctk.CTk):
 
         pd.DataFrame(df).to_csv(file_path, index=False)
 
+    def load_csv(self):
+        if os.path.isfile("data/kpi_datas/recommandation_results.csv"):
+            self.user_pref = pd.read_csv("data/kpi_datas/recommandation_results.csv")
+            self.known_ids = self.known_ids.extend(self.user_pref["0"].tolist())
+        else:
+            self.user_pref = None
+
+        self.tag_engine = tag_engine(self.df_movies, self.movies_tags, self.user_pref, self.known_ids)
+    # ===================================== #
+
+    # ===================================== #
     def dislike_btn(self):
-        self.recommandation_results.append([self.current_movie_id, False, "random"])
-        self.known_ids.append(self.current_movie_id)
+        self.recommandation_results.append([self.current_recommandation[0], False, self.current_recommandation[3]])
+        self.known_ids.append(self.current_recommandation[0])
         self.write_to_csv()
         self.get_new_movie()
 
     def like_btn(self):
-        self.recommandation_results.append([self.current_movie_id, True, "random"])
-        self.known_ids.append(self.current_movie_id)
+        self.recommandation_results.append([self.current_recommandation[0], True, self.current_recommandation[3]])
+        self.known_ids.append(self.current_recommandation[0])
         self.write_to_csv()
         self.get_new_movie()
 
     def unknown_btn(self):
-        self.known_ids.append(self.current_movie_id)
+        self.known_ids.append(self.current_recommandation[0])
         self.get_new_movie()
+    # ===================================== #
 
 
 if __name__ == '__main__':
