1db2d19f2c36936f43bda2aea64de32830e83285
lostiedekerhor <meriadecdekerhor@gmail.com>
2024-03-09 23:43:32 +0100

    - ajout de la recommandation par tag - ajout d'une mesure de surveillance de l'efficacité du model de recommandation

--- recomendation_engine/recomandation_engine.py
+++ recomendation_engine/recomandation_engine.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from collections import Counter, defaultdict
 
 
 class recommandation_engine:
@@ -13,7 +14,6 @@ class recommandation_engine:
 
 
 class random_engin(recommandation_engine):
-
     def __init__(self, datas: pd.DataFrame):
         super().__init__()
         self.datas = datas
@@ -23,3 +23,54 @@ class random_engin(recommandation_engine):
 
     def predict_selection(self, selection_size=10):
         return self.datas.sample(n=selection_size)
+
+
+class tag_engine(recommandation_engine):
+    def __init__(self, datas, movies_tags, recommandations_results, known_ids):
+        super().__init__()
+
+        self.datas = datas
+        self.movies_tags = movies_tags
+        self.user_tags = self.get_user_tags(recommandations_results)
+
+        self.known_ids = known_ids
+
+        self.selection = self.create_selection()
+
+    def get_user_tags(self, liked_movies_df):
+        if not isinstance(liked_movies_df, pd.DataFrame):
+            return []
+
+        liked_tags = []
+        for movie_id, liked, _ in liked_movies_df.values:
+            if liked:
+                tags = self.movies_tags.get(movie_id, [])
+                liked_tags.extend(tags)
+        return liked_tags
+
+    def predict(self):
+        for s in self.selection:
+            if s not in self.known_ids:
+                return s
+
+    def predict_selection(self, selection_size=10):
+        res = []
+        for s in self.selection:
+            if s not in self.known_ids:
+                res.append(s)
+
+        return res[:selection_size]
+
+    def create_selection(self):
+        liked_tags_freq = Counter(self.user_tags)
+
+        movie_scores = defaultdict(int)
+        for movie_id, tags in self.movies_tags.items():
+            for tag in tags:
+                if tag in liked_tags_freq:
+                    movie_scores[movie_id] += liked_tags_freq[tag]
+
+        recommended_movies = sorted(movie_scores.keys(), key=lambda x: movie_scores[x], reverse=True)
+
+        return recommended_movies
+
